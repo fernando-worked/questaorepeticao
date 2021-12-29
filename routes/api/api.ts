948975d3577fe2b27af2cb772c6e074d9078687c
fernandomoreira <fernando.worked@gmail.com>
2021-12-29 17:38:56 -0300

    removido !

--- routes/api/api.ts
+++ routes/api/api.ts
@@ -15,11 +15,7 @@ router.post('/novoUsuario', async (req, res) => {
     if (!EmailValidator.validate(req.body.txemail))
         return res.status(406).send('E-mail inválido!');
     if (await emailJaCadastrado(req.body.txemail)) {
-<<<<<<< HEAD
         return res.status(406).send('E-mail já cadastrado!');  
-=======
-        return res.status(406).send('E-mail já cadastrado!!!'); 
->>>>>>> 70933e0a6515155abca8cc69e50d662bdbfde652
     }
 
     const usuario = new Usuario(req.body.txemail, req.body.txsenha);
