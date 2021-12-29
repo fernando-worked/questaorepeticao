c5f0ab1dea6b4869b0c897baf6b4c4d39b7228c9
fernandomoreira <fernando.worked@gmail.com>
2021-12-29 18:00:59 -0300

    novo teste

--- routes/api/api.ts
+++ routes/api/api.ts
@@ -8,14 +8,14 @@ const router = Router();
 
 router.get('/', (req, res) => {
     res.status(200).send('API ON');
-}) 
+})
 
-
+// teste
 router.post('/novoUsuario', async (req, res) => {
     if (!EmailValidator.validate(req.body.txemail))
         return res.status(406).send('E-mail inválido!');
     if (await emailJaCadastrado(req.body.txemail)) {
-        return res.status(406).send('E-mail já cadastrado!');  
+        return res.status(406).send('E-mail já cadastrado!');
     }
 
     const usuario = new Usuario(req.body.txemail, req.body.txsenha);
@@ -24,7 +24,7 @@ router.post('/novoUsuario', async (req, res) => {
 
     return res.status(201).send('API ON');
 
-}) 
+})
 
 
 export default router;  
