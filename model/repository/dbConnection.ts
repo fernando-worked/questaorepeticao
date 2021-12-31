7072692986e9766d35787bde0d23e8ebb768dcfc
fernandomoreira <fernando.worked@gmail.com>
2021-12-30 21:27:49 -0300

    tratando docker compose

--- model/repository/dbConnection.ts
+++ model/repository/dbConnection.ts
@@ -1,15 +1,16 @@
 import { Pool } from 'pg';
 
 export const poolDB = new Pool({
-    host: 'localhost',
-    database: 'testes',
+    host: process.env.DB_HOST,
+    database: 'testes', 
     port: 5432,
     user: 'postgres',
-    password: 'F@tec123', 
+    password: 'F@tec123',  
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
-}) 
- 
 
+})  
+   
+ 
 module.exports = { poolDB }; 
